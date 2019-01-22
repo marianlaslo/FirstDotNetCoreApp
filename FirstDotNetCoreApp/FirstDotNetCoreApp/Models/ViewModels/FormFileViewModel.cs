848e47bf4c1b8461c431af087c612fb9278ba0db
Marian <marian.laslo@wirtek.com>
2019-01-22 09:49:51 +0200

    Added viewmodels

---
+++ FirstDotNetCoreApp/FirstDotNetCoreApp/Models/ViewModels/FormFileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstDotNetCoreApp.Models.ViewModels
+{
+    public class FormFileViewModel
+    {
+        public string Name { get; set; }
+
+        public string ContentType { get; set; }
+
+        public byte[] Data { get; set; }
+
+        public long Length { get; set; }
+    }
+}
---
+++ FirstDotNetCoreApp/FirstDotNetCoreApp/Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstDotNetCoreApp.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+    }
+}
