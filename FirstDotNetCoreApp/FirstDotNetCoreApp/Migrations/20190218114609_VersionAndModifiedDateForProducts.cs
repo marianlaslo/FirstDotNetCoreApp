653ccd5a577aa9ebf8ed52ae04df9871b8f7e212
Marian Laslo <marian.laslo@wirtek.com>
2019-02-18 13:53:40 +0200

    Added migrations for Products and Files trigger (Version and ModifiedDate columns)

---
+++ FirstDotNetCoreApp/FirstDotNetCoreApp/Migrations/20190218114609_VersionAndModifiedDateForProducts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstDotNetCoreApp.Migrations
+{
+    public partial class VersionAndModifiedDateForProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE TRIGGER trVersionAndModifiedDateForProducts\r\n  " +
+                                 "on Products\r\n\r\n  " +
+                                 "AFTER UPDATE\r\n  " +
+                                 "AS \r\n  " +
+                                 "BEGIN\r\n\t  " +
+                                 "UPDATE Products SET Version = Version + 1,\r\n\t  " +
+                                 "ModifiedDate = GETDATE()\r\n\t  " +
+                                 "WHERE Id IN (SELECT Id FROM inserted)\r\n  " +
+                                 "END\r\n  " +
+                                 "GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER trVersionAndModifiedDateForProducts");
+        }
+    }
+}
---
+++ FirstDotNetCoreApp/FirstDotNetCoreApp/Migrations/20190218114743_VersionAndModifiedDateForFiles.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FirstDotNetCoreApp.Migrations
+{
+    public partial class VersionAndModifiedDateForFiles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE TRIGGER trVersionAndModifiedDateForFiles\r\n  " +
+                                 "on Files\r\n\r\n  " +
+                                 "AFTER UPDATE\r\n  " +
+                                 "AS \r\n  " +
+                                 "BEGIN\r\n\t  " +
+                                 "UPDATE Files SET Version = Version + 1,\r\n\t  " +
+                                 "ModifiedDate = GETDATE()\r\n\t  " +
+                                 "WHERE Id IN (SELECT Id FROM inserted)\r\n  " +
+                                 "END\r\n  " +
+                                 "GO");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER trVersionAndModifiedDateForFiles");
+        }
+    }
+}
